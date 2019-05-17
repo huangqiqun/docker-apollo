7416a10cd9f51edcbb3247726bafab8c9235fa1a
huangqiqun <huangqiqun@126.com>
2019-05-17 18:23:06 +0800

    first commit

---
+++ apollo/scripts/build.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# apollo config db info
+apollo_config_db_url=jdbc:mysql://mycat:8066/ApolloConfigDB?characterEncoding=utf8
+apollo_config_db_username=root
+apollo_config_db_password=root
+
+# apollo portal db info
+apollo_portal_db_url=jdbc:mysql://mycat:8066/ApolloPortalDB?characterEncoding=utf8
+apollo_portal_db_username=root
+apollo_portal_db_password=root
+
+# meta server url, different environments should have different meta server addresses
+dev_meta=http://apollo-configservice-dev:8080
+fat_meta=http://apollo-configservice-fat:8080
+uat_meta=http://apollo-configservice-uat:8080
+pro_meta=http://apollo-configservice-pro:8080
+
+META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
+
+# =============== Please do not modify the following content =============== #
+# go to script directory
+cd "${0%/*}"
+
+cd ..
+
+# package config-service and admin-service
+echo "==== starting to build config-service and admin-service ===="
+
+mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=$apollo_config_db_url -Dspring_datasource_username=$apollo_config_db_username -Dspring_datasource_password=$apollo_config_db_password
+
+echo "==== building config-service and admin-service finished ===="
+
+echo "==== starting to build portal ===="
+
+mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=$apollo_portal_db_url -Dspring_datasource_username=$apollo_portal_db_username -Dspring_datasource_password=$apollo_portal_db_password $META_SERVERS_OPTS
+
+echo "==== building portal finished ===="
---
+++ apollo/scripts/copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ../apollo-configservice/target/apollo-configservice-1.3.0-github.zip ../apollo-configservice/src/main/docker/
+cp ../apollo-adminservice/target/apollo-adminservice-1.3.0-github.zip  ../apollo-adminservice/src/main/docker/
+cp ../apollo-portal/target/apollo-portal-1.3.0-github.zip ../apollo-portal/src/main/docker/
